883abe0eccee976c11d46a7c3e7008ec68324c9a
Diablo <Diablo@malo.com>
2018-10-16 09:43:58 +0800

    抽取App的创建逻辑到业务逻辑模块里面

---
+++ info/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+# 可以用来指定 session 保存的位置
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from redis import StrictRedis
+
+from config import Config
+
+app = Flask(__name__)
+
+# 加载配置
+app.config.from_object(Config)
+
+# 初始化数据库
+db = SQLAlchemy(app)
+
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 开启当前项目 CSRF 保护
+CSRFProtect(app)
+
+Session(app)
--- manage.py
+++ manage.py
@@ -1,27 +1,7 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from flask_wtf.csrf import CSRFProtect
-from redis import StrictRedis
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-# 可以用来指定 session 保存的位置
-from flask_session import Session
-from config import Config
-
-app = Flask(__name__)
-
-# 加载配置
-app.config.from_object(Config)
-
-# 初始化数据库
-db = SQLAlchemy(app)
-
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-# 开启当前项目 CSRF 保护
-CSRFProtect(app)
-
-Session(app)
-
+from flask import session
+from info import app, db
 manager = Manager(app)
 # 将 app 与 db 关联
 Migrate(app, db)
@@ -31,6 +11,7 @@ manager.add_command('db', MigrateCommand)
 
 @app.route('/')
 def index():
+    session['name'] = 'itheima'
     return 'index'
 
 

db7997a09a263498846376d383d81a379e8a8fb1
Diablo <Diablo@malo.com>
2018-10-21 20:48:01 +0800

    获取首页新闻数据后台逻辑实现

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,8 +1,60 @@
-from flask import render_template, current_app, session
+from flask import render_template, current_app, session, request, jsonify
 # from info import redis_store
+from info.constants import CLICK_RANK_MAX_NEWS
 from info.models import User, News
+from info.utils.response_code import RET
 from . import index_blu
 
+
+
+@index_blu.route('/news_list')
+def news_list():
+    '''
+    获取首页新闻数据
+    :return:
+    '''
+    # 1.获取参数
+    # 新闻的分类id
+    cid = request.args.get('cid', '1')
+    page = request.args.get('page', '1')
+    per_page = request.args.get('per_page', '10')
+    # 2.校验参数
+    try:
+        page = int(page)
+        cid = int(cid)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(error=RET.PARAMERR, errmsg='参数错误')
+
+    filters = []
+    if cid != 1: # 查询的不是最新的数据
+        # 需要添加条件
+        filters.append(News.category_id == cid)
+    # 3.查询数据
+    try:
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(error=RET.DBERR, errmsg='数据查询错误')
+    # 取到当前页的数据
+    news_model_list = paginate.items
+    total_page = paginate.pages
+    current_page = paginate.page
+    # 将模型对象列表转换成字典
+    new_dict_li = []
+    for news in news_model_list:
+        new_dict_li.append(news.to_basic_dict())
+
+    data = {
+        'total_page':total_page,
+        'current_page':current_page,
+        'news_dict_li': new_dict_li,
+    }
+
+
+    return jsonify(error=RET.OK, errmsg='ok', data = data)
 @index_blu.route('/')
 def index():
     # session['name'] = 'itheima'
@@ -28,7 +80,7 @@ def index():
     # 右侧新闻排行的逻辑
     news_list = []
     try:
-        news_list = News.query.order_by(News.clicks.desc()).limit(10)
+        news_list = News.query.order_by(News.clicks.desc()).limit(CLICK_RANK_MAX_NEWS)
     except Exception as e:
         current_app.logger.error(e)
     # 定义一个空的字典列表，里面装的就是字典
